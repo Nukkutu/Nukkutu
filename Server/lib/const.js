0ca2d74c2b8ab58d8dcd631e642ddbdefbbaac26
soshimee <45235204+soshimee@users.noreply.github.com>
2022-10-14 12:24:06 +0900

    add special theme

--- Server/lib/const.js
+++ Server/lib/const.js
@@ -206,10 +206,10 @@ exports.KO_INJEONG = [
 	"MOB", "HYK", "CYP", "HRH", "STA", "OIJ",
 	"KGR", "ESB", "ELW", "OIM", "OVW", "NEX", /*"WOW",*/
 	"YRY", "KPO", "JLN", "JAN", "ZEL", "POK", "HAI",
-	"HSS", "KMV", "HDC", "HOS", "MNC"
+	"HSS", "KMV", "HDC", "HOS", "MNC", "SPC"
 ];
 exports.EN_INJEONG = [
-	"LOL", "MNC"
+	"LOL", "MNC", "SPC"
 ];
 exports.KO_THEME = [
 	"30", "40", "60", "80", "90",
